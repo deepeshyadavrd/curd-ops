f83ca8a0f540bdba58bda4494f0c54fc1594eeb3
deepeshyadavrd <raoshsbh6@gmail.com>
2024-06-07 15:05:34 +0530

    gnnn

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
-  return (
-    <div>App</div>
+  const [input, setInput] = useState("")
+  return( 
+    <main>
+    <h1 className="title">CRUD Operations</h1>
+    <div className="input_holder">
+      <input type="text" value={input} onChange={(e) => setInput(e.target.value)}/>
+      <button type="submit">Add Task</button>
+
+    </div>
+    <ul>
+      <List />
+    </ul>
+  </main>
   )
 }
 
