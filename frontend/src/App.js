0d2204ca62164c27f41666dd88d6f55bbe0587f4
deepeshyadavrd <raoshsbh6@gmail.com>
2024-06-11 17:47:02 +0530

    data from mongodb fetched

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,39 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import List from "./components/List";
+import axios from "axios";
+import { baseURL } from "./utils/constant";
 
 const App = () => {
   const [input, setInput] = useState("")
+  const [tasks, setTasks] = useState([])
+  const [updateUI, setUpdateUI] = useState(false)
+
+  useEffect(() => {
+    axios.get(`${baseURL}/get`)
+    .then((res) => {
+      console.log(res.data)
+      setTasks(res.data)
+    })
+  }, [updateUI])
+
+  const addTask = () => {
+    axios.post(`${baseURL}/save`, {task: input}).then((res) => {
+      console.log(res.data);
+      setInput("");
+    })
+  }
   return( 
     <main>
     <h1 className="title">CRUD Operations</h1>
     <div className="input_holder">
       <input type="text" value={input} onChange={(e) => setInput(e.target.value)}/>
-      <button type="submit">Add Task</button>
+      <button type="submit" onClick={addTask}>Add Task</button>
 
     </div>
     <ul>
-      <List task="something"/>
+      {tasks.map((task) => (
+        <List key={task._id} id={task._id} task={task.task} setUpdateUI={setUpdateUI} />
+      ))}
     </ul>
   </main>
   )
